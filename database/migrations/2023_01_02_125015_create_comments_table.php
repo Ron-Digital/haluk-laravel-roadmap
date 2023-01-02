5f50c0adb416f482ee86706376743c435411362e
Haluk Yel <67504077+TallerThanThem@users.noreply.github.com>
2023-01-02 16:05:55 +0300

    first tables created

--- database/migrations/2023_01_02_125015_create_comments_table.php
+++ database/migrations/2023_01_02_125015_create_comments_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      * @return void
      */
     public function up()
-    {
+    {    //
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id')->references('id')->on('posts')->onDelete('cascade')->onUpdate('cascade');
